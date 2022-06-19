a30825031cbadce6da9be5907c44d33dd04171ad
JoelJospell <90523033+JoelJospell@users.noreply.github.com>
2022-06-19 21:07:41 +0300

    Create main.cpp

---
+++ server/main.cpp
@@ -0,0 +1,29 @@
+#include "system.h"
+#include "event.h"
+#include "camera.h"
+#include "generator.h"
+
+#include <iostream>
+
+int main()
+{   
+	std::cout << "Hello, docker_opencv!!!" << std::endl;
+	
+	//system init
+	System* solauticSystem = new System(new Camera(), new Generator(new UDPExchanger('s')));	//uses udp command exchange
+	//System* solauticSystem = new System(new Camera(), new DummyGenerator());	//uses console input
+	
+	//event monitor init
+	IEvent* eventReceiver = new ConsoleEvent();
+	Event event = Event::NONE;
+	
+	//get it into event parser
+	while (event != Event::EXIT)
+	{
+		event = eventReceiver->getEvent();
+		
+		solauticSystem->processEvent(event);
+	}
+	
+    return 0;
+}
