3c4b18edd24135c73ea432793d4f5a44f06afd48
JoelJospell <90523033+JoelJospell@users.noreply.github.com>
2022-06-19 21:04:53 +0300

    Create camera.cpp

---
+++ server/camera.cpp
@@ -0,0 +1,14 @@
+#include "camera.h"
+
+Camera::Camera() //: camera(cv::VideoCapture(0))
+{}
+
+//cv::Mat
+int Camera::getPhoto()
+{
+	int frame;
+	//cv::Mat frame;
+	//camera >> frame;
+	
+	return frame;
+}
